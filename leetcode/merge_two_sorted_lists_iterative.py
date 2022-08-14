c9424f3a505809250e43cc9eb086517bb9c84f3c
genkikadomatsu <genki.kado@gmail.com>
2022-08-13 16:13:07 -1000

    Adding some problems.

--- leetcode/merge_two_sorted_lists_iterative.py
+++ leetcode/merge_two_sorted_lists_iterative.py
@@ -9,50 +9,29 @@ class ListNode:
 class Solution:
     def mergeTwoLists(self, list1:ListNode, list2: ListNode) -> ListNode:
         
-        # Trivial Case
-        if not list1:
-            return list2
-        elif not list2:
-            return list1
         
-        p1, p2 = list1, list2
-        new_head = ListNode()
+        if (list1 is None): return list2
+        if (list2 is None): return list1
+
+        head = ListNode()
+        tail = head
+        a, b = list1, list2
         
-        # Set new head
-        if p1.val <= p2.val:
-            new_head = p1
-        else:
-            new_head = p2
-
-        # Returns the node whose pointer we should reassign
-        # and the node who should be the new p1 or p2
-        def f(node, val):
-            temp = None
-            while (node) and (node.val <= val):
-                temp = node
-                node = node.next
-            return temp, node
-                
-        while p1 or p2:
-
-            if p1.val <= p2.val:
-                to_switch, new_p = f(p1, p2.val)
-                to_switch.next = p2
-                if new_p:
-                    p1 = new_p
-                else:
-                    return new_head
-
-            else:
-                to_switch, new_p = f(p2, p1.val)
-                to_switch.next = p1
-                if new_p:
-                    p2 = new_p
-                else:
-                    return new_head
+        while (a is not None) and (b is not None):
             
+            if a.val < b.val:
+                tail.next = a
+                a = a.next
+                if a is None: tail.next.next = b
+            else:
+                tail.next = b
+                b = b.next
+                if b is None: tail.next.next = a
+            
+            tail = tail.next
+        
+        return head.next
 
-        return new_head
 
 x = ListNode(1)
 x.next = ListNode(2)
@@ -65,7 +44,7 @@ y.next.next = ListNode(4)
 z = ListNode(5)
 
 sol = Solution()
-temp = sol.mergeTwoLists(z, x)
+temp = sol.mergeTwoLists(z, y)
 
 while temp:
     print(temp.val)
