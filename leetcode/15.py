d2e2c71a4135961669b92af30bd9f9d714dd9074
genkikadomatsu <genki.kado@gmail.com>
2023-04-21 20:59:22 -0400

    Updating 15 (3sum).

--- leetcode/15.py
+++ leetcode/15.py
@@ -1,11 +1,23 @@
+from typing import List
+
 class Solution:
+
     def threeSum(self, nums: List[int]) -> List[List[int]]:
 
-
-        nums.sort() #nlogn
+        nums.sort()
+        print("sorted:", nums)
         result = []
 
         for i in range(len(nums) - 2):
+            
+            # If the current element is a duplicate of the previous, skip
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            # If this elemen is positive, 0 sum isn't possible
+            if nums[i] > 0:
+                break
+
 
             a = i
             b = a + 1
@@ -19,14 +31,15 @@ class Solution:
                 else:
                     result.append([nums[a], nums[b], nums[c]])
                     b += 1
+                    c -= 1
+
                     while nums[b] == nums[b - 1] and b < c:
                         b += 1
-                    
 
-        
+                    # This is unecessary but more explicit, debatable...
+                    while nums[c] == nums[c + 1] and b < c:
+                        c -= 1
+                    
         return result
 
 
-input1 = [-1,0,1,2,-1,-4]
-input2 = [0,1,1]
-input3 = [0,0,0]
