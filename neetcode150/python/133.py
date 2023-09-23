ad0739002283a9745040b04cc42e54e7eea4f8ce
genkikadomatsu <genki.kado@gmail.com>
2023-09-23 16:43:33 -0400

    Add LC 133

--- neetcode150/python/133.py
+++ neetcode150/python/133.py
@@ -1,6 +1,9 @@
 """133 Clone Graph
 
 Given a node in an UNDIRECTED connected graph, return a deep copy of the graph.
+
+In this python context, a deep copy is equivalent by value rather than identity.
+A deep copy is it's own distinct object in memory.
 """
 
 from typing import Optional
@@ -13,37 +16,35 @@ class Node:
         self.neighbors = neighbors if neighbors is not None else []
 
 class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        """Return a deep copy of a graph given a reference to a Node.
+    """There are several viable optimal solutions for this."""
+
+    def cloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
+        """Return a deep copy of a grpah given a reference to a Node.
+
+        Perform DFS on the graph while making copies of the original.
 
         Args:
             node (Node): A node in a connected graph.
         Returns:
-            Node: The copy of the original node (in the deep copy graph).
+            Node: The copy of the original node (in the deep copy graph). 
         """
 
         if not node:
             return None
 
-        new_root = Node(node.val)  
         dfs_stack = [node]
-        original_to_copies = {node: new_root}
+        new_root = Node(node.val)
+        old_to_new = {node: new_root}
 
         while dfs_stack:
-            current = dfs_stack.pop()
-
-            for neighbor in current.neighbors:
-                if neighbor not in original_to_copies:
-                    new_copy = Node(neighbor.val)
-                    original_to_copies[neighbor] = new_copy
-                    original_to_copies[current].neighbors.append(new_copy)
+            current_node = dfs_stack.pop()
+            
+            for neighbor in current_node.neighbors:
+               
+                if neighbor not in old_to_new:
                     dfs_stack.append(neighbor)
-                else:
-                    # Copy already exists
-                    original_to_copies[current].neighbors.append(original_to_copies[neighbor]) 
+                    old_to_new[neighbor] = Node(neighbor.value)
+
+                old_to_new[current_node].neighbors.append(old_to_new[neighbor])
 
         return new_root
-
-            
-
- 
