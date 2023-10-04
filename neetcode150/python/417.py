8d75ab6c91b246f7b0e7eb0d9206a69399bdb482
genkikadomatsu <genki.kado@gmail.com>
2023-10-04 19:19:16 -0400

    Adding optimal 417 solution

--- neetcode150/python/417.py
+++ neetcode150/python/417.py
@@ -2,68 +2,50 @@
 from typing import List
 
 class Solution:
-    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        """
-        Returns a list of coordinates that represent all spaces on the island
-        where rain water can reach both the Pacific and Atlantic Oceans.
-        """
+    def optimal_pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        """Optimal solution that has linear average case time complexity."""
 
+        # Working the matrix
         m, n = len(heights), len(heights[0])
+        first_row = [(0, i) for i in range(n)]
+        last_row = [(m - 1, i) for i in range(n)]
+        first_column = [(i, 0) for i in range(m)]
+        last_column = [(i, n - 1) for i in range(m)]
 
-        def can_reach(x: int, y: int) -> bool:
-            """Returns True if (x, y) can flow to both oceans."""
+        # Get all elements initially touching oceans
+        pacific_flow = set(first_row + first_column)
+        atlantic_flow = set(last_row + last_column)
 
-            reaches_pacific = x == 0 or y == 0
-            reaches_atlantic = x == m - 1 or y == n - 1
-
-            dfs_stack = [(x, y)]
-            visited = set()
+        # DFS to find other elements that are touching either
+        def dfs(dfs_stack, visited_set): 
+            """Explore any nodes that are of equal or taller height."""
 
             while dfs_stack:
-                x, y = dfs_stack.pop()
-               
+                i, j = dfs_stack.pop()
                 neighbors = [
-                    (x - 1, y),
-                    (x + 1, y),
-                    (x, y + 1),
-                    (x, y - 1)
+                    (i + 1, j),
+                    (i - 1, j),
+                    (i, j + 1),
+                    (i, j - 1),
                 ]
+                in_range = lambda x, y: (0 <= x < m) and (0 <= y < n)
+                flowing = lambda x, y: heights[x][y] >= heights[i][j]
+                not_visited = lambda x, y: (x, y) not in visited_set
 
-                for neighbor in neighbors:
-                    nx, ny = neighbor
-                    if (
-                        0 <= nx < m and
-                        0 <= ny < n and
-                        neighbor not in visited and
-                        heights[nx][ny] <= heights[x][y]
-                    ):
-                        if nx == 0 or ny == 0:
-                            reaches_pacific = True
-                       
-                        if nx == m - 1 or ny == n - 1:
-                            reaches_atlantic = True
-                        
-                        visited.add(neighbor)
-                        dfs_stack.append(neighbor)
+                for x, y in neighbors:
+                    if not_visited(x, y) and in_range(x, y) and flowing(x, y):
+                        visited_set.add((x, y))
+                        dfs_stack.append((x, y))
+                
+        dfs(list(pacific_flow), pacific_flow)
+        dfs(list(atlantic_flow), atlantic_flow)
 
-            return reaches_pacific and reaches_atlantic
+        return pacific_flow.intersection(atlantic_flow)
 
-        result = [] 
 
-        for i in range(m):
-            for j in range(n):
-                print(i, j)
-                if can_reach(i, j):
-                    result.append([i, j])
+    def naive_pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        """Naive solution that has O(n^2) time complexity.
 
-    
-        return result
-"""417 Pacific Atlantic Water Flow"""
-from typing import List
-
-class Solution:
-    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        """
         Returns a list of coordinates that represent all spaces on the island
         where rain water can reach both the Pacific and Atlantic Oceans.
         """
