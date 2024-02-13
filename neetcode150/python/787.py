e768485022ff4c906cb2221cc742322dd23e64fe
genkikadomatsu <genki.kado@gmail.com>
2024-02-12 19:30:04 -0500

    Update 787 solution

--- neetcode150/python/787.py
+++ neetcode150/python/787.py
@@ -1,10 +1,18 @@
 """787"""
 from typing import List
-import heapq
-
+from math import inf
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
 
+        costs = [0 if node == src else inf for node in range(n)]
 
+        for _ in range(k + 1):
+            frozen_costs = costs.copy()
 
-        
+            for source, destination, cost in flights:
+                new_cost = frozen_costs[source] + cost
+
+                if new_cost < frozen_costs[destination] and new_cost < costs[destination]:
+                    costs[destination] = frozen_costs[source] + cost
+ 
+        return costs[dst] if costs[dst] != inf else -1
