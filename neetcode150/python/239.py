9a702f17b7aedead373a7790dddd3ea0508cc66f
genkikadomatsu <genki.kado@gmail.com>
2024-02-14 12:49:13 -0500

    Update 239 solution

--- neetcode150/python/239.py
+++ neetcode150/python/239.py
@@ -1,29 +1,27 @@
-import collections
+from collections import deque
 from typing import List
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        output = []
-        q = collections.deque()  # index
-        l = r = 0
-        # O(n) O(n)
+
+        result = []
+        q = deque()
+        l, r = 0, 0
+
         while r < len(nums):
-            # pop smaller values from q
+            
             while q and nums[q[-1]] < nums[r]:
                 q.pop()
+            
             q.append(r)
 
-            # remove left val from window
             if l > q[0]:
                 q.popleft()
 
-            if (r + 1) >= k:
-                output.append(nums[q[0]])
+            if r + 1 >= k:
+                result.append(nums[q[0]]) 
                 l += 1
-            r += 1
-            print([nums[i] for i in q])
-            print("out", output)
 
-        return output
+            r += 1                
 
-print(Solution().maxSlidingWindow([3,1,2,0], 3))
+        return result
