3126c3feb20af7cea3c50a07d16e1398a23360a3
genkikadomatsu <genki.kado@gmail.com>
2024-03-20 00:02:38 -0400

    Update 98 solution

--- neetcode150/python/98.py
+++ neetcode150/python/98.py
@@ -8,13 +8,15 @@ class TreeNode:
 
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-
         def recurse(node, min_bound, max_bound):
-            return ((not node)
-                    or ((min_bound < node.val < max_bound)
-                        and (not (node.left and node.left.val > node.val))
-                        and (not (node.right and node.val > node.right.val))
-                        and (recurse(node.left, min_bound, node.val)
-                            and recurse(node.right, node.val, max_bound))))
-        
+            return (
+                (not node) or
+                (
+                    (min_bound < node.val < max_bound) and
+                    (not (node.left and node.left.val > node.val)) and
+                    (not (node.right and node.val > node.right.val)) and
+                    (recurse(node.left, min_bound, node.val) and
+                    recurse(node.right, node.val, max_bound))
+                )
+            )
         return recurse(root, float("-inf"), float("inf")) 
