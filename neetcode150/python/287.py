2d2152d5356a18186d964e78eed1a89b42c0beba
genkikadomatsu <genki.kado@gmail.com>
2024-03-21 01:52:05 -0400

    Fix 287

--- neetcode150/python/287.py
+++ neetcode150/python/287.py
@@ -2,12 +2,12 @@ from typing import List
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
 
-        tortoise, hare = 0, nums[0] # the list's length is >= 2
+        tortoise, hare = 0, 0 # the list's length is >= 2
 
-        while tortoise != hare:
+        while tortoise != hare or tortoise == 0:
             tortoise, hare = nums[tortoise], nums[nums[hare]]
         
-        anchor = nums[0]
+        anchor = 0
 
         while anchor != tortoise:
             anchor, tortoise = nums[anchor], nums[tortoise]
