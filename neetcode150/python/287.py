fb091144c20c72767bb99e93d932ea7ab69b60ee
genkikadomatsu <genki.kado@gmail.com>
2024-03-21 01:39:44 -0400

    Update 287 solution

--- neetcode150/python/287.py
+++ neetcode150/python/287.py
@@ -1,22 +1,15 @@
 from typing import List
-
-# LeetCode 287
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
 
-        tortoise, hare = nums[0], nums[nums[0]] 
+        tortoise, hare = 0, nums[0] # the list's length is >= 2
 
         while tortoise != hare:
-            tortoise = nums[tortoise]
-            hare = nums[nums[hare]]
+            tortoise, hare = nums[tortoise], nums[nums[hare]]
         
-        new_tortoise = 0
-       
-        while tortoise != new_tortoise:
-            tortoise = nums[tortoise]
-            new_tortoise = nums[new_tortoise] 
+        anchor = nums[0]
+
+        while anchor != tortoise:
+            anchor, tortoise = nums[anchor], nums[tortoise]
         
         return tortoise
-
-while True:
-    print(Solution().findDuplicate([int(n) for n in input("list:").split()]))
